69b800d23e7a8d77c4928c341c46a87783eae9c1
Nadar <git@nadar.io>
2023-10-26 08:00:30 +0000

    debuginfo

--- src/Widgets/DebugInfoWidget.php
+++ src/Widgets/DebugInfoWidget.php
@@ -16,6 +16,6 @@ class DebugInfoWidget extends Widget
         $version = YII_ENV_PROD ? Module::getVersionApi()->getVersion() : '-';
         $lastUpdate = YII_ENV_PROD ? date("d.m.Y H:i", Module::getVersionApi()->getUpdatedAt()) : '-';
 
-        return "<!-- " . implode('|', ["debug: {$debug}", "env: {$env}", "release: {$release}", "version: {$version}", "version date: {$lastUpdate}"]) . " -->";
+        return "<!-- " . implode(' | ', ["debug:{$debug}", "env:{$env}", "release:{$release}", "version:{$version}", "version date:{$lastUpdate}"]) . " -->";
     }
 }
