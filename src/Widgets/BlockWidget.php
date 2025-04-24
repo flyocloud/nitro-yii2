6bbc6fb5bb6cd8e3de40e68449acb4aaa879c760
Basil Suter <git@nadar.io>
2025-04-24 15:41:28 +0000

    Refactor BlockWidget to handle missing component names with error handling

--- src/Widgets/BlockWidget.php
+++ src/Widgets/BlockWidget.php
@@ -3,6 +3,7 @@
 namespace Flyo\Yii\Widgets;
 
 use Flyo\Model\Block;
+use yii\base\InvalidConfigException;
 use yii\base\Widget;
 
 class BlockWidget extends Widget
@@ -11,6 +12,16 @@ class BlockWidget extends Widget
 
     public function run()
     {
+        $viewFile = $this->block->getComponent();
+
+        if (empty($viewFile)) {
+            if (YII_DEBUG) {
+                throw new InvalidConfigException("Block component name which is responsible for rendering the block is not set.");
+            }
+
+            return '';
+        }
+
         return $this->render('@app/views/flyo/'. $this->block->getComponent(), [
             'block' => $this->block,
         ]);
